88c5578c59906e42da57e46f965583485c65406f
Samadrita Karmakar <sam@sam-laptop>
2020-07-18 08:32:51 +0530

    added to parts to export in RapidFEM.jl file

--- src/RapidFEM.jl
+++ src/RapidFEM.jl
@@ -1,4 +1,12 @@
 module RapidFEM
 include("FEM/feSpace.jl")
-
+#From FEM
+export AbstractElement, LineElement, TriElement, QuadElement, TetElement, HexElement
+export createFeSpace, feSpace!, get_∂x_∂ξ, getFunction_dΩ, getFunction_∂ξ_∂x, getInterpolated_x
+#From Mesh
+export Mesh, readMesh, getNoOfElements, getCoordArray
+#From Quadrature
+export getQuadrature
+#From ShapeFunction
+export IpPoint, ShapeFunction, calculateShapeFunctions
 end # module
