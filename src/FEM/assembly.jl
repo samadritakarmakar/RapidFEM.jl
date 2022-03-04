a309f8bf9ab1aa13197cba594f2b1370e8ab9a2f
Samadrita Karmakar <samadritakarmakar@gmail.com>
2022-03-04 16:56:32 +0100

    Modified assembleMatrix! function to work with coupled and mixed finite element problems

--- src/FEM/assembly.jl
+++ src/FEM/assembly.jl
@@ -23,17 +23,57 @@ function getRange(RangeDict::Dict{Array{Int64,1}, StepRange{Int64,Int64}}, activ
     return RangeDict[activeDimensions]
 end
 
+function getProblemType(meshTuple::Tuple{Mesh, Mesh}, problemDimTuple::Tuple{Int64, Int64})
+    if meshTuple[1] == meshTuple[2] && problemDimTuple[1] == problemDimTuple[2]
+        return :General
+    elseif meshTuple[1] == meshTuple[2]
+        return :Coupled
+    else
+        return :Mixed
+    end  
+end
+
+function getElements(problemType::Symbol, attribute::Tuple{Int64, Int64}, meshTuple::Tuple{Mesh, Mesh}, elementNo::Int64)
+    
+    element_2::AbstractElement =  meshTuple[2].Elements[attribute][elementNo]
+    if problemType == :Mixed
+        element_1::AbstractElement = meshTuple[1].Elements[attribute][elementNo]
+        return element_1, element_2
+    end
+    return element_2, element_2
+end
+
+function getReductionFix(problemType::Symbol, element_1::AbstractElement, element_2::AbstractElement)
+
+    if problemType != :Mixed
+        return 0, 0
+    end
+    #By default element_2 is the set order. If element_1 has higher order, then that is used.
+    element = element_2
+    order = element_2.order
+    if element_1.order > element_2.order
+        element = element_1
+        order = element_1.order          
+    end
+    #Fix order based on: order_max = order_i - reduction_fix_i; reduction_fix_i is negative since a negative reduction means an increase in order.
+    reduction_fix_1 = element_1.order - order
+    reduction_fix_2 = element_2.order - order
+    return reduction_fix_1, reduction_fix_2
+end
+
 """Responsible for assembling of FEM Matrices. paramerterFunction could
 be anything depending on what you would like to send to the local matrix
-assembler. The below example is similar to used in: src/Examples/poisson.jl
+assembler. This function is to be used if the Matrix shall be Mixed and Coupled.
 This function is useful if the local matrix makes changes to the parameters variable.
 
-    K::SparseMatrixCSC = assembleMatrix!(parameters, volAttrib, FeSpace, mesh, RapidFEM.local_∇v_λ_∇u!, problemDim, activeDimensions)
+    K::SparseMatrixCSC = assembleMatrix!(parameters, attribute, FeSpace, (mesh1, mesh2), localMatrixFunc, (problemDim1, problemDim2),
+    activeDimensions, varArgs...; reduction = reduction, elementFunction=lagrange, quadrature= gauss)
 """
 function assembleMatrix!(parameters::T, attribute::Tuple{Int64, Int64},
     FeSpace::Dict{Tuple{DataType, Int64, Any, Int64}, Array{ShapeFunction}},
-    mesh::Mesh,  localMatrixFunc::Function, problemDim::Int64,
-    activeDimensions::Array{Int64,1}=[1, 1, 1], varArgs...; reduction::Int64 = 0, elementFunction::Function=lagrange, quadrature::Function = gauss)::SparseMatrixCSC where T
+    meshTuple::Tuple{Mesh, Mesh}, localMatrixFunc::Function, problemDimTuple::Tuple{Int64, Int64},
+    activeDimensions::Array{Int64,1}=[1, 1, 1], varArgs...; 
+    reduction::Int64 = 0, elementFunction::Function=lagrange, quadrature::Function = gauss)::SparseMatrixCSC where T   
 
     numOfThreads::Int64 = Threads.nthreads()    #Total number of threads running
     #An array of SparseMatrixCOO is used to addup matrices of in each thread
@@ -47,37 +87,56 @@ function assembleMatrix!(parameters::T, attribute::Tuple{Int64, Int64},
     FeSpaceThreaded::Array{Dict{Tuple{DataType, Int64, Any, Int64}, Array{ShapeFunction}}, 1} =
     Array{Dict{Tuple{DataType, Int64, Any, Int64}, Array{ShapeFunction}}, 1}(undef, numOfThreads)
     FeSpaceThreaded[1] = FeSpace #First thread can be the same as the original FeSpace
+    
     Threads.@threads for thread ∈ 2:numOfThreads
         FeSpaceThreaded[thread] = deepcopy(FeSpace) #Others need to be deepcopies
     end
     RangeDict = createDimRange() # Creates a dim range. Useful when using for 1D or 2D problems
     dimRange::StepRange{Int64,Int64} = getRange(RangeDict, activeDimensions)
+
+    problemType = getProblemType(meshTuple, problemDimTuple)
+
     #######This makes sure the full size of K_COO is same as the size of the filled K matrix
-    vNodes2::Array{Int64,1} = [mesh.noOfNodes*problemDim]
+    vNodes2_1::Array{Int64,1} = [meshTuple[1].noOfNodes*problemDimTuple[1]]
+    vNodes2_2::Array{Int64,1} = [meshTuple[2].noOfNodes*problemDimTuple[2]]
     K_local2::Array{Float64,2} = zeros(1,1)
     ####
     Threads.@threads for thread ∈ 1:numOfThreads #Setting the matrices size for each thread
-        FEMSparse.assemble_local_matrix!(K_COO[thread], vNodes2, vNodes2, K_local2)
+        FEMSparse.assemble_local_matrix!(K_COO[thread], vNodes2_1, vNodes2_2, K_local2)
     end
     ### Multi-Thread Assembly starts here
-    Threads.@threads for elementNo ∈ 1:length(mesh.Elements[attribute])
+    Threads.@threads for elementNo ∈ 1:length(meshTuple[2].Elements[attribute])
         currentThread::Int64 = Threads.threadid()
-        element::AbstractElement = mesh.Elements[attribute][elementNo]
+        #Element Initializations
+        element_1, element_2 = getElements(problemType, attribute, meshTuple, elementNo)
         #if the type of element changes then reallocate the local matrix else replace with zeros
-        if length(K_localArray[currentThread]) != problemDim*element.noOfElementNodes
-            K_localArray[currentThread] = zeros(problemDim*element.noOfElementNodes,problemDim*element.noOfElementNodes)
+        if size(K_localArray[currentThread]) != (problemDimTuple[1]*element_1.noOfElementNodes, problemDimTuple[2]*element_2.noOfElementNodes)
+            K_localArray[currentThread] = zeros(problemDimTuple[1]*element_1.noOfElementNodes, problemDimTuple[2]*element_2.noOfElementNodes)
         else
             fill!(K_localArray[currentThread],0.0)
         end
-        #K_localArray[currentThread] = zeros(problemDim*element.noOfElementNodes,problemDim*element.noOfElementNodes)
-        coordArrayTemp::Array{Float64,2} = getCoordArray(mesh, element)
-        coordArray::Array{Float64,2} = coordArrayTemp[dimRange,:]
-        shapeFunction::Array{ShapeFunction,1} = feSpace!(FeSpaceThreaded[currentThread], element, mesh, reduction = reduction, 
+        reduction_fix_1, reduction_fix_2 = getReductionFix(problemType, element_1, element_2)
+        shapeFunction_2::Array{ShapeFunction,1} = feSpace!(FeSpaceThreaded[currentThread], element_2, meshTuple[2], reduction = (reduction + reduction_fix_2), 
         elementFunction = elementFunction, quadrature = quadrature)
-        
-        localMatrixFunc(K_localArray[currentThread], parameters, problemDim, element, elementNo, shapeFunction, coordArray, varArgs...)
-        vNodes::Array{Int64} = getVectorNodes(element, problemDim)
-        FEMSparse.assemble_local_matrix!(K_COO[currentThread], vNodes, vNodes, K_localArray[currentThread])
+        #Arguments for localMatrixFunc
+        coordArrayTemp2::Array{Float64,2} = getCoordArray(meshTuple[2], element_2)
+        coordArray2::Array{Float64,2} = coordArrayTemp2[dimRange,:]
+        if problemType == :General
+            localMatrixFunc(K_localArray[currentThread], parameters, problemDimTuple[2], element_2, elementNo, shapeFunction_2, coordArray2, varArgs...)
+        elseif problemType == :Coupled
+            localMatrixFunc(K_localArray[currentThread], parameters, problemDimTuple[1], problemDimTuple[2], element_2, elementNo, shapeFunction_2, coordArray2, varArgs...)
+        elseif problemType == :Mixed
+            shapeFunction_1::Array{ShapeFunction,1} = feSpace!(FeSpaceThreaded[currentThread], element_1, meshTuple[1], reduction = (reduction + reduction_fix_1), 
+                elementFunction = elementFunction, quadrature = quadrature)
+            coordArrayTemp1::Array{Float64,2} = getCoordArray(meshTuple[1], element_1)
+            coordArray1::Array{Float64,2} = coordArrayTemp1[dimRange,:]
+            localMatrixFunc(K_localArray[currentThread], parameters, problemDimTuple[1], problemDimTuple[2], element_1, element_2, elementNo, shapeFunction_1, shapeFunction_2, coordArray1, coordArray2, varArgs...)
+        else
+            error("Unknown Problem Type!")
+        end
+        vNodes_1::Array{Int64} = getVectorNodes(element_1, problemDimTuple[1])
+        vNodes_2::Array{Int64} = getVectorNodes(element_2, problemDimTuple[2])
+        FEMSparse.assemble_local_matrix!(K_COO[currentThread], vNodes_1, vNodes_2, K_localArray[currentThread])
     end
     ### Multi-Thread Assembly ends here
     for thread ∈ 2:numOfThreads ### Adding all the thread matrices to 1st K_COO
@@ -96,15 +155,48 @@ This function is useful if the local matrix makes changes to the parameters vari
 
     K::SparseMatrixCSC = assembleMatrix!(parameters, volAttrib, FeSpace, mesh, RapidFEM.local_∇v_λ_∇u!, problemDim, activeDimensions)
 """
+function assembleMatrix!(parameters::T, attribute::Tuple{Int64, Int64},
+    FeSpace::Dict{Tuple{DataType, Int64, Any, Int64}, Array{ShapeFunction}},
+    mesh::Mesh, localMatrixFunc::Function, problemDim::Int64,
+    activeDimensions::Array{Int64,1}=[1, 1, 1], varArgs...; 
+    reduction::Int64 = 0, elementFunction::Function=lagrange, quadrature::Function = gauss)::SparseMatrixCSC where T
+
+    return assembleMatrix!(parameters, attribute, FeSpace, (mesh, mesh), localMatrixFunc, (problemDim, problemDim),
+    activeDimensions, varArgs...; reduction = reduction, elementFunction=elementFunction, quadrature= quadrature)
+end
+
+"""Responsible for assembling of FEM Matrices. paramerterFunction could
+be anything depending on what you would like to send to the local matrix
+assembler. The below example is similar to used in: src/Examples/poisson.jl
+This function is useful if the local matrix makes changes to the parameters variable.
+
+    K::SparseMatrixCSC = assembleMatrix!(parameters, volAttrib, FeSpace, mesh, RapidFEM.local_∇v_λ_∇u!, problemDim, activeDimensions)
+"""
 function assembleMatrix(parameters::T, attribute::Tuple{Int64, Int64},
     FeSpace::Dict{Tuple{DataType, Int64, Any, Int64}, Array{ShapeFunction}},
     mesh::Mesh,  localMatrixFunc::Function, problemDim::Int64,
     activeDimensions::Array{Int64,1}=[1, 1, 1], 
     varArgs...; reduction::Int64 = 0, elementFunction::Function=lagrange, quadrature::Function = gauss)::SparseMatrixCSC where T
 
-    return assembleMatrix!(parameters, attribute,
-        FeSpace, mesh, localMatrixFunc, problemDim, activeDimensions, varArgs..., ; 
-        reduction = reduction, elementFunction = elementFunction, quadrature = quadrature)
+    return assembleMatrix!(parameters, attribute, FeSpace, mesh,  localMatrixFunc, problemDim,
+    activeDimensions, varArgs...; reduction = reduction, elementFunction = elementFunction, quadrature = quadrature)
+end
+
+"""Responsible for assembling of FEM Matrices. paramerterFunction could
+be anything depending on what you would like to send to the local matrix
+assembler. This function is to be used if the Matrix shall be Coupled.
+This function is useful if the local matrix makes changes to the parameters variable.
+
+    K::SparseMatrixCSC = assembleMatrix!(parameters, volAttrib, FeSpace, mesh, RapidFEM.local_∇v_λ_∇u!, problemDim, activeDimensions)
+"""
+function assembleMatrix!(parameters::T, attribute::Tuple{Int64, Int64},
+    FeSpace::Dict{Tuple{DataType, Int64, Any, Int64}, Array{ShapeFunction}},
+    mesh::Mesh, localMatrixFunc::Function, problemDimTuple::Tuple{Int64, Int64},
+    activeDimensions::Array{Int64,1}=[1, 1, 1], varArgs...; 
+    reduction::Int64 = 0, elementFunction::Function=lagrange, quadrature::Function = gauss)::SparseMatrixCSC where T
+
+    return assembleMatrix!(parameters, attribute, FeSpace, (mesh, mesh), localMatrixFunc, problemDimTuple,
+    activeDimensions, varArgs...; reduction = reduction, elementFunction=elementFunction, quadrature= quadrature)
 end
 
 """Responsible for assembling of FEM Vectors. parameters could
--- src/FEM/coupledComponents.jl
+++ src/FEM/coupledComponents.jl
@@ -1,3 +1,12 @@
+"""CoupledComponents is used in a global sense and is useful if coupled problems are being used. 
+The problem may also be a mixed finite element problem. The struct stores the global end position of solution.
+c[i], where is a CoupledComponents variable and i the i_th solution variable, outputs a UnitRange m:n.
+Here m is the starting global index and n is the end global index of the given solution variable[i] in 
+a global FEM vector
+Same can be done for a global FEM matrix c[i,j] which outputs (m_i:n_i,m_j:n_j) where i and j are the 
+i_th and j_th respectively solution variables.
+"""
+
 struct CoupledComponents
     dofs::Array{Int64, 1}
     function CoupledComponents(mesh::Mesh, problemDims::Array{Int64, 1})
@@ -11,7 +20,7 @@ struct CoupledComponents
         return new(dofs)
     end
 
-    function CoupledComponents(meshTuple::Tuple, problemDims::Array{Int64, 1}, meshNos::Array{Int64, 1})
+    function CoupledComponents(meshTuple::NTuple{T1, Mesh}, problemDims::NTuple{T2, Int64}, meshNos::NTuple{T3, Int64}) where {T1, T2, T3}
         @assert length(problemDims) == length(meshNos) "Length of problemDims must be the same as meshNos."
         @assert maximum(meshNos) <= length(meshTuple) "Maximum of meshNos cannot be greater than the total number of meshes."
 
@@ -25,6 +34,7 @@ struct CoupledComponents
     end
 end
 
+
 function Base.getindex(c::CoupledComponents, i::Integer)
     lastIndex = i == 1 ? 1 : c.dofs[i-1]+1
     @assert i <=length(c.dofs) "Attempt to access $(length(c.dofs))-length at index $i."
@@ -33,4 +43,14 @@ end
 
 function Base.getindex(c::CoupledComponents, i::Integer, j::Integer)
     return c[i], c[j]
+end
+
+"""Creates a global sparse zero matrix from the CoupledComponents struct variable"""
+function SparseArrays.spzeros(c::CoupledComponents)
+    return spzeros(c.dofs[end], c.dofs[end])
+end
+
+"""Creates a global dense zero vector from the CoupledComponents struct variable"""
+function Base.zeros(c::CoupledComponents)
+    return zeros(c.dofs[end])
 end
